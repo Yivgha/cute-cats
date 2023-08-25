575d5f6c57c02c43af1ed79047d514c3d8f47f00
quicklab <genieukne@gmail.com>
2023-08-25 17:58:42 +0300

    feat: basic configs and favicon change

---
+++ catapi/src/app/layout.js
@@ -0,0 +1,18 @@
+import './globals.css'
+
+
+export const metadata = {
+  title: 'Cute cats API',
+  description: 'Look for best cat ever',
+  icons: {
+    icon: './icon.ico',
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body >{children}</body>
+    </html>
+  )
+}
